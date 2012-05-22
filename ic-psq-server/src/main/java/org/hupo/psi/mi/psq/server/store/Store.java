620d628aeb7e967ad0e92cb61438d3b540fc13ce
lukasz <lukasz@d25c7308-2bfa-47b1-9105-34c40392dc46>
2012-05-22 01:23:09 +0000

    PSICQUIC repo rearragement

--- ic-psq-server/src/main/java/org/hupo/psi/mi/psq/server/store/Store.java
+++
@@ -1,8 +0,0 @@
-package org.hupo.psi.mi.psq.server.store;
-
-import java.util.List;
-
-public interface Store{
-    public String getRecord( String id, String format );
-    public List<String> getRecordList( List<String> id, String format );
-}
--- ic-psq-server/src/main/webapp/index.jsp
+++
@@ -1,2 +0,0 @@
-<h1>PSICQUIC</h1>
-<hr/>
