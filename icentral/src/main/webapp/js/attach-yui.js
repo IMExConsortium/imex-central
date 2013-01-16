c57ff381d7ef978dec6a95913469b3e777cabd15
zplat <zplat@d25c7308-2bfa-47b1-9105-34c40392dc46>
2013-01-16 19:09:11 +0000

    fixed a bug with uploading no files was not checked. also added update to attachment number when adding only, so far.

--- icentral/src/main/webapp/js/attach-yui.js
+++ icentral/src/main/webapp/js/attach-yui.js
@@ -247,8 +247,10 @@ YAHOO.imex.attedit = {
 		for(var i = 0;i < tabs.length; i++)
 		{
 			currentLabel = tabs[i].get('label');
-			if(currentLabel == label)
+			if(currentLabel.indexOf(label) >= 0)
 			{
+				if(currentLabel.indexOf('(') > 0)
+					currentLabel = currentLabel.slice(0, currentLabel.indexOf('(') - 1);
 				tabs[i].set('label', currentLabel +' (' + count+ ')' );
 				return
 			}
@@ -263,15 +265,18 @@ YAHOO.imex.attedit = {
         var nameFld = YAHOO.util.Dom.get( o.nf );
         var fileFld = YAHOO.util.Dom.get( o.ff );
         //nothing in the name field
-        if( nameFld.value == null || nameFld.value == '' ){
-            nameFld.value = fileFld.files[0].name;
-        } 
-        //same value in the name field as what will be inserted
-        else if(fileFld.files[0].name == nameFld.value){}
-        //insert the file name
-        else {   
-            nameFld.value = nameFld.value + " (" + fileFld.files[0].name +")";
-        }
+        if(typeof fileFld.files[0] != "undefined")
+        {
+			if( nameFld.value == null || nameFld.value == '' ){
+				nameFld.value = fileFld.files[0].name;
+			} 
+			//same value in the name field as what will be inserted
+			else if(fileFld.files[0].name == nameFld.value){}
+			//insert the file name
+			else {   
+				nameFld.value = nameFld.value + " (" + fileFld.files[0].name +")";
+			}
+		}
     },
 
     pubPreview: function( aclass, op ){
@@ -395,17 +400,23 @@ YAHOO.imex.attedit = {
 		if(fileField.files[0].size + textField.value.length < 2095000 && fileField.files.length > 0)
 		{
 */
-		var btn = YAHOO.util.Dom.get('attmgr_op_eada');
-		var form = new FormData(formElement);
-		var xhr = new XMLHttpRequest();
-		form.append(btn.name, btn.value);
-		xhr.open("POST", "/icentral/attachmgr", false);
-		xhr.send(form);
-		formElement.reset();
-		if(xhr.status != 404)
-			YAHOO.imex.attedit.attachReload({'argument':'adata'});
-		else
-			alert('Error during file upload. Check if file is too large');
-
+		if(typeof fileField.files[0] != "undefined")
+		{
+			var btn = YAHOO.util.Dom.get('attmgr_op_eada');
+			var form = new FormData(formElement);
+			var xhr = new XMLHttpRequest();
+			form.append(btn.name, btn.value);
+			xhr.open("POST", "/icentral/attachmgr", false);
+			xhr.send(form);
+			formElement.reset();
+			if(xhr.status != 404)
+			{
+				var edit = YAHOO.imex.attedit
+				edit.attachReload({'argument':'adata'});
+				edit.addTitleCount(edit.conf['adata'].comTable.getRecordSet().getLength() + 1, 'Attachments')
+			}
+			else
+				alert('Error during file upload. Check if file is too large');
+		}
 	}
 };
